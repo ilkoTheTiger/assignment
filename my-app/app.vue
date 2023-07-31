bba01462c6d49af21f452cc0f3ad0e24004a09be
ilkoTheTiger <ilkothetiger@gmail.com>
2023-07-31 17:56:07 +0300

    Initialized Nuxt3 project and added sample home page

---
+++ my-app/app.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <header>
+      <nav>
+        <li><a href="/home" class="">Home</a></li>
+        <li><a href="/posts" class="">Home</a></li>
+        <li><a href="/home" class="">Home</a></li>
+      </nav>
+    </header>
+    <main>
+      <HomePage />
+    </main>
+
+  </div>
+</template>
+
+
+<script>
+    import HomePage from '~/pages/home.vue'
+
+    export default {
+        components: {
+            HomePage
+        }
+    }
+</script>
