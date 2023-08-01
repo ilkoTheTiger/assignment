1a03312bd3c497a2f40e40afa81fdc2c91b46578
ilkoTheTiger <ilkothetiger@gmail.com>
2023-08-01 13:25:20 +0300

    Posts data fetched from the API

--- my-app/app.vue
+++ my-app/app.vue
@@ -1,26 +1,32 @@
+<script setup>
+const { data: posts } = await useFetch(
+  "https://jsonplaceholder.typicode.com/posts/"
+);
+
+// Importing Navigation and Footer Components
+import Navigation from "~/components/navigation.vue";
+import FooterComponent from "~/components/footer.vue";
+</script>
+
 <template>
   <div class="flex flex-col h-screen justify-between">
-    <header class="border-b bg-gradient-to-r from-sky-500 to-indigo-500 border-gray-300 mb-3">
+    <header
+      class="border-b bg-gradient-to-r from-sky-500 to-indigo-500 border-gray-300 mb-3"
+    >
       <Navigation />
     </header>
     <main class="font-serif p-6 mb-auto">
       <NuxtPage />
+      <div v-for="post in posts">
+        <ul>
+          <li>
+            {{ posts }}
+          </li>
+        </ul>
+      </div>
     </main>
     <footer>
       <FooterComponent />
     </footer>
   </div>
 </template>
-
-<script>
-// Importing Navigation and Footer Components
-import Navigation from "~/components/navigation.vue";
-import FooterComponent from "~/components/footer.vue";
-
-export default {
-  components: {
-    Navigation,
-    FooterComponent,
-  },
-};
-</script>
