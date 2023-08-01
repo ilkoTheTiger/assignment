f3253b58dfe7d9eb25390ca6cd287d57926224dd
ilkoTheTiger <ilkothetiger@gmail.com>
2023-08-01 11:58:32 +0300

    Added padding and increased the font size in the Footer

--- my-app/components/footer.vue
+++ my-app/components/footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="border-b bg-gradient-to-r from-sky-500 to-indigo-500 border-gray-300 mt-3 text-white flex justify-center">
+  <div class="border-b bg-gradient-to-r from-sky-500 to-indigo-500 border-gray-300 mt-3 p-2 text-lg text-white flex justify-center justify-items-center">
     All Rights Reserved! &copy; 2023
   </div>
 </template>
