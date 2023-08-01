1aa58b3c3015718352ada004725c6cc60aaea942
ilkoTheTiger <ilkothetiger@gmail.com>
2023-08-01 19:38:43 +0300

    Navigation Links Font-Size increased to be closer to the other componets Font-Sizes and Hover Effect added

--- my-app/components/navigation.vue
+++ my-app/components/navigation.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     <nav class="max-w-6xl mx-8 py-4 px-1 xl:px-1 flex flex-start">
-      <ul class="flex items-center space-x-4 pr-6 text-lg text-white">
+      <ul class="flex items-center space-x-4 pr-6 text-xl text-white">
         <li>
-          <nuxt-link to="/home">Home</nuxt-link>
+          <nuxt-link to="/home" class="hover:text-gray-100">Home</nuxt-link>
         </li>
         <li>
-          <nuxt-link to="/posts">Posts</nuxt-link>
+          <nuxt-link to="/posts" class="hover:text-gray-100">Posts</nuxt-link>
         </li>
       </ul>
     </nav>
