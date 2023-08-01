a58d3ab8129ed315aa79bbd06e94602af4aac0a3
ilkoTheTiger <ilkothetiger@gmail.com>
2023-08-01 16:09:31 +0300

    Added Details View Page - [id].vue  for Posts inside the pages/posts dir

---
+++ my-app/pages/posts/[id].vue
@@ -0,0 +1,13 @@
+<script setup>
+const {id} = useRoute().params
+
+const { data: post } = await useFetch(
+  `https://jsonplaceholder.typicode.com/posts/${id}`
+);
+// const id = ref(1);
+</script>
+
+<template>
+  <h1>{{ $route.params.id }}</h1>
+  {{ post }}
+</template>
