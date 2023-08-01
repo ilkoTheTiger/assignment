95147032303bf1333bf6de071d4e19c9daee712b
ilkoTheTiger <ilkothetiger@gmail.com>
2023-08-01 17:51:14 +0300

    Created LatestPost component to for the Home Page and added hard-coded content to it for visualization purposes

---
+++ my-app/components/LatestCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <article class="post w-full p-7 rounded-xl">
+    <h2 class="w-full text-2xl mb-2 text-gray-700 flex justify-between">
+      <span>Check our our Latest Blog Posts</span>
+    </h2>
+    <div v-for="num in 3" class="backdrop-blur-2xl rounded-xl py-3 px-5 my-4">
+      <h2 class="w-full text-2xl mb-2 text-gray-700 flex justify-between">
+        <span>Lorem ipsum? Dolore?</span>
+        <span>#{{ 541 - num }}</span>
+      </h2>
+      <p class="mt-1 text-gray-600 text-xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
+        officia, nesciunt dicta ipsam culpa natus suscipit accusamus ipsa nam
+        vel harum possimus quod cupiditate quam ratione, alias veniam dolore id.
+      </p>
+    </div>
+  </article>
+</template>
--- my-app/pages/index.vue
+++ my-app/pages/index.vue
@@ -1,6 +1,23 @@
+<script setup>
+// Importing Navigation and Footer Components
+import LatestCard from "~/components/LatestCard.vue";
+</script>
+
 <template>
-  <div>
-    <!-- This page correctly has only one single root element -->
-    <h1 class="text-3xl font-bold underline">Index Page!</h1>
-  </div>
+  <section
+    class="backdrop-blur-2xl bg-white/80 max-w-6xl mx-auto flex flex-wrap justify-center py-6 px-6 xl:px-0 p-3"
+  >
+    <h2 class="w-full text-4xl text- flex justify-center justify-items-center">
+      Welcome to our Blog!
+    </h2>
+
+    <LatestCard />
+    
+    <NuxtLink
+      to="/posts"
+      class="mt-6 inline-block px-8 py-4 rounded-xl bg-blue-500 text-white text-xl hover:bg-blue-700"
+    >
+      See All Posts
+    </NuxtLink>
+  </section>
 </template>
