532e5cc064842de10c2fb2686e6359d0a7458a30
ilkoTheTiger <ilkothetiger@gmail.com>
2023-08-01 16:55:16 +0300

    Renamed Footer.vue as it was detected as an auto-import and made the Footer Sticky, ensuring it is always on the bottom by passing min-h-screen to the <main> container

--- my-app/app.vue
+++ my-app/app.vue
@@ -1,22 +1,24 @@
 <script setup>
 // Importing Navigation and Footer Components
-import Navigation from "~/components/navigation.vue";
-import FooterComponent from "~/components/footer.vue";
+import Navigation from "~/components/Navigation.vue";
+import FooterComponent from "~/components/FooterComponent.vue";
 </script>
 
 <template>
-  <div class="flex flex-col h-screen justify-between">
+  <div
+    class="h-max flex flex-col h-screen justify-between bg-gradient-to-r from-sky-500 to-indigo-500"
+  >
     <header
       class="border-b bg-gradient-to-r from-sky-400 to-indigo-400 border-gray-200"
     >
       <Navigation />
     </header>
-    <main
-      class="font-serif p-6 mb-auto bg-gradient-to-r from-sky-500 to-indigo-500"
-    >
+
+    <main class="min-h-screen font-serif p-6 mb-auto">
       <NuxtPage />
     </main>
-    <footer>
+    
+    <footer class="sticky bottom-0">
       <FooterComponent />
     </footer>
   </div>
